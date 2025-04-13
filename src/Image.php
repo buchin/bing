67d6d852bd3edbb1a284bac30379951821d0d149
Mochammad Masbuchin <buchin@dropsugar.com>
2025-04-13 08:13:20 +0700

    Fix: Bing Image new structure

--- src/Image.php
+++ src/Image.php
@@ -9,6 +9,7 @@ class Image extends Bing
 {
     public $prefix = "images/async";
     public $raw_images, $related;
+    public $images = [];
 
     public function getContent()
     {
@@ -29,27 +30,40 @@ class Image extends Bing
 
     public function parseContent()
     {
-        $results = $this->crawler
-            ->filter(".imgpt")
-            ->each(function (Crawler $node, $i) {
-                $json = @json_decode($node->filter("a.iusc")->attr("m"));
-                try {
-                    $size = $node->filter("div.img_info span.nowrap")->html();
-                } catch (\InvalidArgumentException $e) {
-                    // I guess its InvalidArgumentException in this case
-                    $size = "0 x 0";
-                }
+        $results = $this->crawler->filter(".imgpt")->each(function ($node) {
+            $json = @json_decode($node->filter("a.iusc")->attr("m"));
 
-                $raw_image = [
+            if(is_null($json)){
+                return null;
+            }
+
+            try {
+                $size = $node->filter("div.img_info span.nowrap")->html();
+            } catch (\InvalidArgumentException $e) {
+                // I guess its InvalidArgumentException in this case
+                $size = "0 x 0";
+            }
+
+            try {
+                return [
+                    "url" => $json->murl,
                     "mediaurl" => $json->murl,
                     "link" => $json->purl,
                     "title" => str_replace(["", "", " ..."], "", $json->t),
                     "thumbnail" => $json->turl,
                     "size" => $size,
+                    "desc" => $json->desc,
                 ];
+            }
+            catch (\Exception $e){
+                return [];
+            }
+        });
 
-                return $raw_image;
-            });
+        $results = collect($results)
+            ->filter()
+            ->unique("url")
+            ->toArray();
 
         $related = $this->crawler
             ->filter("a > div.cardInfo > div > strong")
@@ -79,9 +93,9 @@ class Image extends Bing
 
     public function postProcessSingleImage($raw_image, $delimiter = "·")
     {
-        $raw_image["filetype"] = trim(
-            @explode($delimiter, $raw_image["size"])[1]
-        );
+        $urlparts = @explode($delimiter, $raw_image["url"]);
+
+        $raw_image["filetype"] = $urlparts[count($urlparts)-1];
         $raw_image["filetype"] =
             $raw_image["filetype"] == "jpeg" ? "jpg" : $raw_image["filetype"];
         $raw_image["filetype"] =
@@ -89,14 +103,25 @@ class Image extends Bing
                 ? "gif"
                 : $raw_image["filetype"];
 
-        $raw_image["width"] = explode(
-            " x ",
-            @explode($delimiter, $raw_image["size"])[0]
-        )[0];
-        $raw_image["height"] = explode(
-            " x ",
-            @explode($delimiter, $raw_image["size"])[0]
-        )[1];
+        // Check if filetype is successfully extracted
+        if(str($raw_image["filetype"])->length() > 5){
+            // filetype length is usually 3-5 chars, lets find it from URL;
+            $raw_image["filetype"] = str($raw_image['url'])->afterLast('.')->before('?')->toString();
+        }
+
+        try {
+            $sizeParts = explode($delimiter, $raw_image["size"]);
+            $dimensions = isset($sizeParts[0]) ? explode("×", $sizeParts[0]) : [];
+
+            $raw_image["width"] = isset($dimensions[0]) ? (int) $dimensions[0] : 0;
+            $raw_image["height"] = isset($dimensions[1]) ? (int) $dimensions[1] : 0;
+        }
+        catch (\Exception $e)
+        {
+            $raw_image["width"] = 0;
+            $raw_image["height"] = 0;
+        }
+
         $raw_image["domain"] = parse_url($raw_image["link"], PHP_URL_HOST);
 
         return $raw_image;
